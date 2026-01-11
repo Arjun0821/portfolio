5ccd4f7331f4d7e715b090149bf143afacab6f1d
Arjun0821 <150239376+Arjun0821@users.noreply.github.com>
2026-01-11 21:36:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const texts = [
+  "Aspiring Ethical Hacker...",
+  "Learning Web Security...",
+  "Bug Bounty Beginner...",
+  "Building Secure Websites..."
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type() {
+  if (count === texts.length) {
+    count = 0;
+  }
+
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.getElementById("typing").textContent = letter;
+
+  if (letter.length === currentText.length) {
+    setTimeout(() => {
+      index = 0;
+      count++;
+    }, 1500);
+  }
+
+  setTimeout(type, 100);
+})();
